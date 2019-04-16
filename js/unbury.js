43e8faa3c0271a283c60febf245a66efa528c342
dominic montelongo and Liz Kelley <dominic@>
2019-04-16 16:29:58 -0700

    add unbury button

--- js/unbury.js
+++ js/unbury.js
@@ -1,4 +1,4 @@
-var countdownDate = new Date("April 16, 2019 16:00:00").getTime();
+var countdownDate = new Date("April 17, 2019 16:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -13,34 +13,3 @@ var x = setInterval(function() {
     $("#current-time").html("time capsule OPEN");
   }
 }, 1000);
-
-
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-//
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-//
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-//
-// function showSlides(n) {
-//   var i;
-//   var slides = $(".my-slides");
-//   var dots = $(".dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
