f5062bc2244ee5567b84afb5103919e6ea83490d
dominic montelongo and Stuart McKay <stuart@>
2019-04-17 15:10:25 -0700

    Start BL foundation

--- js/indexScripts.js
+++ js/indexScripts.js
@@ -1,6 +1,24 @@
+// Business Logic
+function passwordConfirm(userPassword, userPasswordConfirm){
+  if ("user-password"==="user-password-confirm") {
+    function
+  }
+}
+
+// User Interface Logic
 $(document).ready(function() {
-  $("button.btn").click(function(){
+  $("button.btn").click(function(){ // Enter Website
     $("div.titleSplash").hide();
     $("main.loginScreen").show();
-  })
+  });
+  $("button.btn-small").click(function(){ // Displays Site Registration Section
+    $("main.loginScreen").hide();
+    $("main.registerScreen").show();
+  });
+  $("#registerNewUser").click(function(){ // Registers and Logs in new User
+    $("main.registerScreen").hide();
+    $("main.loginScreen").hide();
+    var userPassword = $("input#user-password").val();
+    var userPasswordConfirm = $("input#user-password-confirm").val();
+  });
 });
